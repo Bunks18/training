392be2ae3473b1154b58790a86ddade2d7af6aec
Bunks18 <wbunker11@gmail.com>
2018-01-16 17:07:33 -0500

    figure out palindrome

--- js_ex.js
+++ js_ex.js
@@ -3,9 +3,7 @@
 let myVar = [1,3,4,5]
 
 myVar.reverse();
-
 //swap elements 
-
 const swapMe = (array, first, second) => { 
   //create variable to store something
   temp = '';
@@ -15,4 +13,30 @@ const swapMe = (array, first, second) => {
   array[first] = array[second]
   //take second
   array[second] = temp
+
+  console.log(array)
+}
+
+//Write a simple function (less than 160 characters) that 
+//returns a boolean indicating whether or not a string is a palindrome.
+
+//my shit doesnt work
+
+let myVar = 'sssss'
+
+const stringSplit = (obj) => {
+  let objArray = obj.split("");
+  let reverse = objArray.reverse()
+  objArray = objArray.join("")
+  reverse = reverse.join("")
+  if(objArray === reverse) {console.log(1)}
+  else 0
+}
+
+stringSplit(myVar)
+
+//answer 
+function isPalindrome(str) {
+  str = str.replace(/\W/g, '').toLowerCase();
+  return (str == str.split('').reverse().join(''));
 }
