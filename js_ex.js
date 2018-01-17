e0794dd442b17d7022d10c6261cbe844c649de30
Bunks18 <wbunker11@gmail.com>
2018-01-17 14:28:32 -0500

    did palindrome

--- js_ex.js
+++ js_ex.js
@@ -13,30 +13,13 @@ const swapMe = (array, first, second) => {
   array[first] = array[second]
   //take second
   array[second] = temp
-
   console.log(array)
 }
 
 //Write a simple function (less than 160 characters) that 
 //returns a boolean indicating whether or not a string is a palindrome.
 
-//my shit doesnt work
-
-let myVar = 'sssss'
-
-const stringSplit = (obj) => {
-  let objArray = obj.split("");
-  let reverse = objArray.reverse()
-  objArray = objArray.join("")
-  reverse = reverse.join("")
-  if(objArray === reverse) {console.log(1)}
-  else 0
+reverseCheck = (arg) => {
+  return arg.split("").reverse().join("") === arg
 }
 
-stringSplit(myVar)
-
-//answer 
-function isPalindrome(str) {
-  str = str.replace(/\W/g, '').toLowerCase();
-  return (str == str.split('').reverse().join(''));
-}
